2dc548b74f9b81574826eaa4a44369ed8fa61110
Dani Setiawan <mayadani28@gmail.com>
2022-08-07 02:05:21 +0700

    first setup

---
+++ src/main/java/com/yeahbutstill/demospring/bootdesignpatterns/DemoSpringBootDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.demospring.bootdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/demospring/bootdesignpatterns/DemoSpringBootDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.demospring.bootdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
